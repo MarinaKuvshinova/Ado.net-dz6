e7f81c4f49dd3dc859971c420a7e2e1e195b43f4
MarinaKuvshinova <pn-05@ukr.net>
2018-04-05 14:49:13 +0300

    new file

---
+++ 26.03/26.03/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace _26._03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(327, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(297, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 123);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(390, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 69);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(114, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 285);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 26.03/26.03/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace _26._03
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Data Source = HOME-ПК; Initial Catalog = BookShop; Integrated Security = true;";
+        SqlConnection sqlConnection = null;
+        DataTable dataTable = null;
+        public Form1()
+        {
+            InitializeComponent();
+            sqlConnection = new SqlConnection(connectionString);
+            btnStart.Click += btnStart_Click;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            label1.Text = "";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex != -1)
+            {
+                label1.Text = comboBox1.SelectedValue.ToString();
+            }
+        }
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            string sql = textBox1.Text;
+            dataGridView1.DataSource = null;
+            comboBox1.DataSource = null;
+            dataGridView1.DataSource = await GetDataAsync(sql);
+            comboBox1.DataSource = await GetDataComboboxAsync(sql); ;
+            comboBox1.DisplayMember = "Name";
+            comboBox1.ValueMember = "count";
+            label1.Text = comboBox1.SelectedValue.ToString();
+        }
+
+        private async Task<object> GetDataComboboxAsync(string sql)
+        {
+            if (sql == "select * from Books" || sql == "select*from Books" || sql == "select * from books")
+            {
+                using (sqlConnection = new SqlConnection(connectionString))
+                {
+                    await sqlConnection.OpenAsync();
+                    //or sql += " order by ID_Author";
+                    string sql2 = @"select CONCAT(Authors.FirstName,' ', Authors.LastName) as Name, COUNT(Books.ID_Author) as count from Books inner join Authors on Authors.ID_Author=Books.ID_Author group by CONCAT(Authors.FirstName,' ', Authors.LastName)";
+                    SqlCommand sqlCommand = new SqlCommand(sql2, sqlConnection);
+                    dataTable = new DataTable();
+                    SqlDataReader sqlDataReader = await sqlCommand.ExecuteReaderAsync();
+                    int line = 0;
+                    do
+                    {
+                        while (await sqlDataReader.ReadAsync())
+                        {
+                            if (line++ == 0)
+                            {
+                                for (int i = 0; i < sqlDataReader.FieldCount; i++)
+                                {
+                                    dataTable.Columns.Add(sqlDataReader.GetName(i));
+                                }
+                            }
+                            DataRow dataRow = dataTable.NewRow();
+                            for (int i = 0; i < sqlDataReader.FieldCount; i++)
+                            {
+                                dataRow[i] = await sqlDataReader.GetFieldValueAsync<Object>(i);
+                            }
+                            dataTable.Rows.Add(dataRow);
+                        }
+                    } while (await sqlDataReader.NextResultAsync());
+                }
+            }
+            else dataTable = null;
+            return dataTable;
+        }
+
+        private async Task<object> GetDataAsync(string sql)
+        {
+            using (sqlConnection = new SqlConnection(connectionString))
+            {
+                await sqlConnection.OpenAsync();
+                
+                SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection);
+                dataTable = new DataTable();
+                SqlDataReader sqlDataReader = await sqlCommand.ExecuteReaderAsync();
+                int line = 0;
+                do
+                {
+                    while (await sqlDataReader.ReadAsync())
+                    {
+                        if (line++ == 0)
+                        {
+                            for (int i = 0; i < sqlDataReader.FieldCount; i++)
+                            {
+                                dataTable.Columns.Add(sqlDataReader.GetName(i));
+                            }
+                        }
+                        DataRow dataRow = dataTable.NewRow();
+                        for (int i = 0; i < sqlDataReader.FieldCount; i++)
+                        {
+                            dataRow[i] = await sqlDataReader.GetFieldValueAsync<Object>(i);
+                        }
+                        dataTable.Rows.Add(dataRow);
+                    }
+                } while (await sqlDataReader.NextResultAsync());
+                return dataTable;
+            }
+        }
+    }
+}
